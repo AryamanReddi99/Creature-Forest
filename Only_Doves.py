d1b6f84e46b9cdbf0ed2aaa1c6171cf2bd5c7208
Aryaman <aryamanreddi@gmail.com>
2019-09-20 23:57:38 +0530

    Add files via upload

--- Only_Doves.py
+++ Only_Doves.py
@@ -1,24 +1,28 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[5]:
+# In[46]:
 
 
 import import_ipynb
-import Creature_Forest_Base
+from Creature_Forest_Base import *
+import seaborn as sns
 
 
-# In[13]:
+# In[73]:
 
 
 creature_list = []
-for i in range(25):
+for i in range(1):
     creature_list.append(Dove())
 
-generation_count = []
-population_count = []
-for i in range(10):
-    creature_list = run_generation(creature_list, 1000)
+generation_count = [0]
+population_count = [len(creature_list)]
+
+num_generations = 20
+
+for i in range(1,num_generations):
+    creature_list = run_generation(creature_list, carrying_capacity=1000)
     population = len(creature_list)
     population_count.append(population)
     generation_count.append(i)
@@ -26,7 +30,9 @@ for i in range(10):
 print(generation_count)
 print(population_count)
 
-plt.stackplot(generation_count, population_count, labels = ['Doves'])
+pal = sns.color_palette("Set2")
+plt.stackplot(generation_count, population_count, labels = ['Doves'], colors = pal, alpha=0.5)
+plt.grid()
 plt.legend(loc = 'upper left')
 plt.show()
 
